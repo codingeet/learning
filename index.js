1f54ad9e22e2ff4cbbea6c6596792d95ee581960
geetanjali <geet2136@gmail.com>
2024-08-25 11:24:54 +0530

    arithmetic operator used

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let firstname = "john";
-let lastname = "doe";
-let fullname = firstname +" "+ lastname;
-console.log(fullname)
+let x = 7;
+let y = 3;
+let mul = x * y;
+let div = x / y;
+let rem = x % y;
+let expo = x ** y;
+let decrement = --x;
+let increment = x++;
+console.log(x);
+
+
+
+
+
+
+console.log("multiplication of 7and3????", mul);
+console.log(" this is division>>>>>>>", div);
+console.log("this is rem////////", rem);
+console.log(" this is expo<<<<<<",expo);
+console.log("decremental",decrement);
+console.log("incremental",increment);
